9baad4389fefed6f73d9be5b7aea32d4421ae01b
mnabieliparf06 <32686919-mnabieliparf06@users.noreply.replit.com>
2024-03-11 05:17:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# membuat variable
+nama = "Nabiel"
+semester = 2
+kelas = "TI23B"
+jurusan = "Teknik Informatika"
+quote = "Hidup seperti larry"
+married = False
+adalahPerempuan = False
+hobi = 'Nonton, Main Game, Makan'
+
+print("Nama saya adalah ", nama)
+print("Semester ", semester)
+print("Kelas saya ", kelas)
+print("Jurusan saya ", jurusan)
+print("Quote ", quote)
+print("Hobi saya ", hobi)
